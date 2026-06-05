d5c36c40dcd5b09f05ad7a9295e420c3f18d39f8
albertoab2026 <albertoballarta2017@gmail.com>
2026-06-05 05:42:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1037,7 +1037,7 @@ elif menu == "Reportes":
         st.markdown("""
             <style>
             div[data-testid="metric-container"] { background-color: #1e293b; padding: 20px; border-radius: 10px; border: 1px solid #475569; }
-            div[data-testid="metric-container"] [data-testid="stMetricValue"] { font-size: 2.5rem !important; color: #38bdf8!important; }
+            div[data-testid="metric-container"] [data-testid="stMetricValue"] { font-size: 2.5rem; color: #38bdf8; }
             </style>
         """, unsafe_allow_html=True)
 
